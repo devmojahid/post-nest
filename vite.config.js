f020ff03fa41075c4aa5061d05a7e5f955724dbe
devmojahid <raofahmedmojahid@gmail.com>
2024-07-15 15:38:54 +0600

    inisial commit

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: 'resources/js/app.jsx',
+            ssr: 'resources/js/ssr.jsx',
+            refresh: true,
+        }),
+        react(),
+    ],
+    // my app url is APP_URL=http://post-nest.test
+    server: {
+        host: 'post-nest.test',
+    }
+});
